0bca8f00550a713f817aeff352fbdefa8985df33
GabrielGB <gbtav83@gmail.com>
2021-05-19 18:37:05 -0300

    Frist commit if/else

---
+++ script.js
@@ -0,0 +1,61 @@
+
+//1 - Crie uma condição que exiba uma mensagem no console se a idade for maior que 18
+
+//  2 - Crie uma condição que exiba uma mensagem no console se você for maior de idade E a condição humana seja true
+
+//3 - Criar uma condição que exiba uma mensagem no console se você fizer aniversário em Janeiro OU Dezembro
+
+
+//*** Exercícios bônus
+//4 - Criar uma condição que exiba uma mensagem no console se seu nome começar com a letra R
+
+//5 - Criar uma condição que exiba uma mensagem no console se seu sobrenome tenha mais de 6 letras OU seu nome começar com a letra E
+
+
+/*
+let idade = 20
+if (idade > 18 ) {console.log ("Parabéns, você é maior de idade!")}
+else{
+    console.log("Desculpe, você é menor de idade!")
+}
+*/
+
+
+/*
+let idade = 18
+let humano = "true"
+if (idade >= 18 ) {console.log ("Parabéns, você é maior de idade!")
+console.log("Você é humano!" , humano)}
+else{
+    console.log( "Você é menor de idade!")
+    }
+*/
+
+/*
+let mes =01
+if ((mes ===12) || (mes === 01)){
+    console.log("Você faz aniversário em Dezembro ou Janeiro!")
+} else {
+    console.log("Você não faz aniversário em Dezembro, nem em Janeiro!")
+}
+*/
+
+//Bônus//
+
+/*
+let nome = "Gabriel"
+if (nome.substring(0,1) === "R"){
+    console.log("Que legal, Seu nome começa com a letra 'R'!")
+}
+else {console.log("Ah, que pena! Seu nome não começa com a letra 'R'!" )}
+*/
+
+/*
+let sobrenome = "Euclides"
+if ((sobrenome.substring(0, 1) === "E") || (sobrenome.length > 6)) {
+    console.log("Boa," , sobrenome, "começa com a letra 'E' ou tem mais de '6' letras!")
+}
+else {
+    console.log(sobrenome, "não começa com a letra 'E' ou tem menos de '6' letras!")
+}
+*/
